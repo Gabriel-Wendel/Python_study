713d4854e61428f6a2ef9ac6860aa1bb012534be
Gabriel-Wendel <xiolones@gmail.com>
2022-08-02 18:32:51 -0300

    solves: challenge06 & challenge07

--- World_01/challenge06.py
+++ World_01/challenge06.py
@@ -1,3 +1,8 @@
 "Challenge 05 - Curso Python #07"
 #Create an algorithm that reads a number
 #and displays its double, triple, and square root.
+
+firstNumber = float(input('Enter a number:'))
+print(f'The number you entered is {firstNumber}\n \
+Double is {firstNumber*2}\n Triple is {firstNumber*3}\n \
+Square root is {firstNumber**2}')
--- World_01/challenge07.py
+++ World_01/challenge07.py
@@ -1,3 +1,9 @@
 "Challenge 05 - Curso Python #07"
 #Develop a program that reads a student's two grades,
 #calculates and displays their average.
+
+firstGrade = float(input('Enter the value of the first test grade:'))
+secondGrade = float(input('Enter the value of the second test grade:'))
+print(f'The value of the first grade is {firstGrade}\n \
+The value of the second grade is {secondGrade}\n \
+The grade average is {(firstGrade+secondGrade)/2} points')
